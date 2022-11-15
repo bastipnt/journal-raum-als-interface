a2df7ffcc5d0184f92cb7e2753505a5f9b73d4a0
litc0de <litc0de@posteo.org>
2022-11-15 10:07:19 +0100

    setup

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+import "@fontsource/montserrat";
+import "@fontsource/josefin-sans";
+import CustomLayout from "./wrapPageElement";
+import "./src/styles/_global.scss";
+
+export const wrapPageElement = CustomLayout;
---
+++ src/layout/page-layout.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Link, useStaticQuery, graphql } from "gatsby";
+
+import classNames from "classnames/bind";
+import * as styles from "./layout.module.scss";
+
+const cx = classNames.bind(styles);
+
+type Props = {
+  children: any;
+};
+
+const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Helmet
+        htmlAttributes={{
+          lang: "en",
+        }}
+      >
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+      </Helmet>
+      <header className={cx("header")}>
+        <div className={cx("headerContainer")}>
+          <h1 className={cx('pageTitle')}>Raum als Interface</h1>
+          <b>Sebastian Paintner</b>
+        </div>
+      </header>
+
+      <main className={cx("main")}>
+        <div className={cx("mainContainer")}>{children}</div>
+      </main>
+
+      <footer className={cx("footer")}>
+        <div className={cx("footerContainer")}>
+          2022&nbsp;•&nbsp;Sebastian Paintner&nbsp;•&nbsp;UDK Matrikelnummer:&nbsp;4044970
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
